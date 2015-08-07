7dc2152a622693c6ca4607b692cc47d2fc8fb962
Jônatas Bueno do Livramento <bueno_namikazi@hotmail.com>
2015-08-06 23:07:56 -0300

    Interface gráfica introduzida (by Michele)

--- Desenvolvimento/Sistema.Acesso/Sistema.Acesso.View/FormSystem.cs
+++ Desenvolvimento/Sistema.Acesso/Sistema.Acesso.View/FormSystem.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Sistema.Acesso.View
 {
-    public partial class Form1 : Form
+    public partial class FormSystem : Form
     {
-        public Form1()
+        public FormSystem()
         {
             InitializeComponent();
         }
