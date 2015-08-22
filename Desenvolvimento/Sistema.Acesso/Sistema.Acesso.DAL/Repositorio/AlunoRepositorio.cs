468cda289756bd3558eae3aa13bca053f3d5f1d0
Jônatas Bueno do Livramento <bueno_namikazi@hotmail.com>
2015-08-21 23:14:13 -0300

    Deletando Solution
    Preparando para inserir nova solução

--- Desenvolvimento/Sistema.Acesso/Sistema.Acesso.DAL/Repositorio/AlunoRepositorio.cs
+++
@@ -1,9 +0,0 @@
-﻿using Sistema.Acesso.DAL.Repositorio.Base;
-using Sistema.Acesso.Entidades;
-
-namespace Sistema.Acesso.DAL.Repositorio
-{
-    public class AlunoRepositorio : Repositorio<Aluno>
-    {
-    }
-}
--- Desenvolvimento/Sistema.Acesso/Sistema.Acesso.DAL/Repositorio/SolicitacaoSaidaRepositorio.cs
+++
@@ -1,8 +0,0 @@
-﻿using Sistema.Acesso.DAL.Repositorio.Base;
-using Sistema.Acesso.Entidades;
-namespace Sistema.Acesso.DAL.Repositorio
-{
-    public class SolicitacaoSaidaRepositorio : Repositorio<SolicitacaoSaida>
-    {
-    }
-}
